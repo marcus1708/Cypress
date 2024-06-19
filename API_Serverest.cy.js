8fe7a3e583d5d36a53ceec723fb7c7da3aad77b8
Marcus Vinicius <87437572+marcus1708@users.noreply.github.com>
2024-06-19 17:10:31 -0300

    Update API_Serverest.cy.js

--- API_Serverest.cy.js
+++ API_Serverest.cy.js
@@ -1,42 +1,42 @@
 describe('Automaçao API do ServeRest', () => {
     const token = `Bearer ${Cypress.env('ServeRest_access_token')}`
-    let id;  
+    let auth, id;
     it('POST - Cadastra usuário', () => {     
-      cy.request({
-        method: 'POST',
-        url: 'https://serverest.dev/usuarios',
-        body:{
-            "nome": "MARCUS",
-            "email": "qa@teste.com",
-            "password": "teste",
-            "administrador": "true"
-        }
-      }).then((createUserResponse) => {
-        id = createUserResponse.body._id;
-        Cypress.env('Id', id); 
-        expect(createUserResponse.status).to.eq(201)
-        })
-    })
-    it('GET - Busca usuário já cadastrado', () => {
-        cy.request({
-            method: 'GET',
-            url: `https://serverest.dev/usuarios/${id}`
-        }).then((response) => {
-           expect(response.status).to.eq(200)})   
-    })
+        cy.api({
+          method: 'POST',
+          url: 'https://serverest.dev/usuarios',
+          body:{
+              "nome": "QA",
+              "email": "qa@qa.com",
+              "password": "teste",
+              "administrador": "true"
+          }
+        }).then((createUserResponse) => {
+          id = createUserResponse.body._id;
+          Cypress.env('Id', id); 
+          expect(createUserResponse.status).to.eq(201)
+          })
+    }) 
     it('POST - Realiza Login', () => {
-        cy.request({
+        cy.api({
           method: 'POST',
           url: 'https://serverest.dev/login',
           body:{
-              "email": "qa@teste.com",
+              "email": "qa@qa.com",
               "password": "teste"
-          },headers: { token }
+          }
         }).then((response) => {
             expect(response.status).to.eq(200)})  
     })  
+    it('GET - Busca usuário já cadastrado', () => {
+        cy.api({
+            method: 'GET',
+            url: `https://serverest.dev/usuarios/${id}`,
+        }).then((response) => {
+           expect(response.status).to.eq(200)})   
+    })
     it('DELETE - Exclui usuário', () => {
-        cy.request({
+        cy.api({
             method: 'DELETE',
             url: `https://serverest.dev/usuarios/${id}`,
         }).then((response) => {
