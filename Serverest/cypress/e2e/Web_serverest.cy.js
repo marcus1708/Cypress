6e59bd84c508c82bf210a8615d9ff976ed101879
Marcus Vinicius <87437572+marcus1708@users.noreply.github.com>
2024-11-01 15:08:01 -0300

    Update Web_serverest.cy.js

--- Serverest/cypress/e2e/Web_serverest.cy.js
+++ Serverest/cypress/e2e/Web_serverest.cy.js
@@ -1,5 +1,5 @@
 import { faker } from '@faker-js/faker';
-describe('Web - Serverest - Cadastro de usuário', () => {
+describe.skip('Web - Serverest - Cadastro de usuário', () => {
     it('Visita a Home', () => {
         cy.visit('https://front.serverest.dev/login')
         cy.contains('Login').should('be.visible')
@@ -7,7 +7,10 @@ describe('Web - Serverest - Cadastro de usuário', () => {
     it('Cadastra um novo usuário', () => {
         cy.Cadastra_Usuario()
     })
-    it('Teste de Falha - Cadastro de um usuário já cadastrado antes', () => {
+    it('Teste de Falha - Cadastro de um usuário (com intercept)', () => {
+        cy.Cadastra_Usuario_intercept()
+    })
+    it('Teste de Falha - Cadastro de um usuário', () => {
         cy.Cadastra_Usuario_inv()
     })
 })
@@ -20,8 +23,14 @@ describe('Web Serverest - Usuário e Produtos', () => {
     it('Listar Usuário', () => {
         cy.Lista_Usuario()
     })    
-    it.only('Cadastrar Produtos', () => {
+    it('Listar Usuário (com intercept)', () => {
+        cy.Lista_Usuario_intercept()
+    }) 
+    it('Cadastrar Produtos', () => {
         cy.Cadastrar_Produtos()
+    })
+    it('Listar Produtos (com intercept)', () => {
+        cy.Listar_Produtos_intercept()
     })
     it('Listar Produtos', () => {
         cy.Listar_Produtos()
@@ -34,5 +43,10 @@ describe('Web Serverest - Usuário e Produtos', () => {
     })
     it('Excluir Usuário', () => {
         cy.Excluir_Usuário()
+        cy.log("Não é possível excluir o proprio usuário")
+    })
+    it('Excluir Usuário (com intercept)', () => {
+        cy.Excluir_Usuário_intercept()
+        cy.log("Não é possível excluir o proprio usuário")
     })
 })
